7977d779cc0f5d2456fede57586645e4787101d5
Lisa-creates <lizalive56@gmail.com>
2024-04-04 19:44:25 +0300

    Refinement of the side brake light detection function

--- detection_and_classification_brake_lights/brake_lights_detection.cpp
+++ detection_and_classification_brake_lights/brake_lights_detection.cpp
@@ -13,43 +13,48 @@ using namespace std;
 
 
 
-double calculate_I_S(Rect R_i, Rect R_j) {
+double calculate_I_S(cv::Mat stats_i, cv::Mat stats_j, double tao_S) {
+    cv::Rect R_i(stats_i.at<int>(cv::CC_STAT_LEFT), stats_i.at<int>(cv::CC_STAT_TOP), stats_i.at<int>(cv::CC_STAT_WIDTH), stats_i.at<int>(cv::CC_STAT_HEIGHT));
+    cv::Rect R_j(stats_j.at<int>(cv::CC_STAT_LEFT), stats_j.at<int>(cv::CC_STAT_TOP), stats_j.at<int>(cv::CC_STAT_WIDTH), stats_j.at<int>(cv::CC_STAT_HEIGHT));
     Rect intersection = R_i & R_j;
     Rect unionRect = R_i | R_j;
+    if (unionRect.area() != 0) {
+        double I_S = float(intersection.area()) / float(unionRect.area()); 
 
-    double I_S = float(intersection.area()) / float(unionRect.area());
+        /*   if (intersection.area() > 0) {
+                std::cout << "Intersection found: " << intersection << std::endl;
+                cout << intersection.area() << std::endl;
+            }
+            else {
+                std::cout << "No intersection found 1" << std::endl;
+            }
 
-    /*   if (intersection.area() > 0) {
-            std::cout << "Intersection found: " << intersection << std::endl;
-            cout << intersection.area() << std::endl;
-        }
-        else {
-            std::cout << "No intersection found 1" << std::endl;
-        }
+            if (unionRect.area() > 0) {
+                std::cout << "Union found: " << unionRect << std::endl;
+                cout << unionRect.area() << std::endl;
+            }
+            else {
+                std::cout << "No unionRect found 1" << std::endl;
+            }
 
-        if (unionRect.area() > 0) {
-            std::cout << "Union found: " << unionRect << std::endl;
-            cout << unionRect.area() << std::endl;
-        }
-        else {
-            std::cout << "No unionRect found 1" << std::endl;
-        }
+            cout << "I_S " << I_S << std::endl; */ 
 
-        cout << "I_S " << I_S << std::endl; */
-
-    return I_S;
+        if (I_S >= tao_S)
+            return I_S;
+        else
+            return 0; 
+    }
+    else
+        return 0;
 } 
 
-double calculate_I_D(Rect R_i, Rect R_j, vector<Rect> R) {
-    double total_sum = 0;
-    for (auto& rectangle : R)
-        total_sum += rectangle.area(); 
-    double I_D = (R_i.area() + R_j.area()) / total_sum; 
+double calculate_I_D(int area_i, int area_j, int total_sum) {
+    double I_D = (area_i + area_j) / total_sum;
     cout << "I_D " << I_D << endl; 
     return I_D; 
 } 
 
-double calculate_I_U(double u_R_i, double u_R_j, double half_img_weight) {
+double calculate_I_U(int u_R_i, int u_R_j, double half_img_weight) {
     double numerator = min(half_img_weight - u_R_i, u_R_j - half_img_weight);
     double denominator = max(half_img_weight - u_R_i, u_R_j - half_img_weight); 
    // cout << "numerator " << numerator << "denominator " << denominator << endl; 
@@ -57,44 +62,72 @@ double calculate_I_U(double u_R_i, double u_R_j, double half_img_weight) {
     return I_U;
 } 
 
-double calculate_I_lb(Rect Ri, Rect Rj, double half_img_weight, double lambda_S, double lambda_D, double lambda_U, double tao_S, vector<cv::Rect> rectangles) {
-    if (lambda_S + lambda_D + lambda_U != 1 || lambda_U < 0) {
-        cout << "WrongLambdaValue"; 
-        return 1.0;
+double calculate_I_lb(const cv::Mat& stats_i, const cv::Mat& centroids_i, const cv::Mat& stats_j, const cv::Mat& centroids_j, bool& right_and_left, float half_img_weight, double lambda_S, double lambda_D, double lambda_U, double tao_S, int total_sum_rectangles) {
+    const double EPSILON = 1e-6; // Погрешность для сравнения суммы с 1.0
+
+    if (abs(lambda_S + lambda_D + lambda_U - 1.0) > EPSILON || lambda_U < 0) {
+ //   if (lambda_S + lambda_D + lambda_U != 1.0 || lambda_U < 0) {// 
+        cout << "WrongLambdaValue sum: " << lambda_S + lambda_D + lambda_U << endl << lambda_U;
+        return -1.0;
     }
-   
-    else {
-        cv::Rect Ci(0, 0, 1, 10); // Центр прямоугольников  
-        cv::Rect Cj(1, 0, 2, 30); // !!!!!!! 
-        double I_S = calculate_I_S(Ri, Rj);
-        double I_D = calculate_I_D(Ri, Rj, rectangles);
+    else { 
+        double I_S = calculate_I_S(stats_i, stats_j, tao_S);
+        double I_D = calculate_I_D(stats_i.at<int>(4), stats_j.at<int>(4), total_sum_rectangles);
         double I_U = 0;
-        if (Ri.x < Rj.x) {
-            double I_U = calculate_I_U(Ci.x, Cj.x, half_img_weight);
+        if (stats_i.at<int>(0) < stats_i.at<int>(0)) {
+            double I_U = calculate_I_U(centroids_i.at<double>(0), centroids_j.at<double>(0), half_img_weight);
         }
-        double I_lb = lambda_S * I_S + lambda_D * I_D + lambda_U * I_U;
-      //  cout << "I_lb " << I_lb << endl;
+        else {
+       //     right_and_left = true; 
+            double I_U = calculate_I_U(centroids_j.at<double>(0), centroids_i.at<double>(0), half_img_weight);
+        }
+        double I_lb = lambda_S * I_S + lambda_D * I_D + lambda_U * I_U; 
+        cout << "I_lb " << I_lb << endl;
         return I_lb;
     }
 } 
 
+Mat detector_new(const cv::Mat& stats, const cv::Mat& centroids, float half_img_weight) {
 
+    double max_ij = -1.0;
+    int tao_v = 60;
+    double tao_S = 0.3; // Добавить проверку на  >= tao_S
+    double lambda_S = 0.3;
+    double lambda_D = 0.4;
+    double lambda_U = 1 - lambda_S - lambda_D;
 
-void detector(const Mat& image) {
-    cv::Rect Ri(0, 0, image.cols / 3, image.rows / 3);
-    cv::Rect Rj(1, 0, image.cols / 2, image.rows / 2); // Области фар 
-    double tao_S = 0; // Добавить проверку на  >= tao_S
-    double lambda_S = 0.2; 
-    double lambda_D = 0.3; 
-    double lambda_U = 1 - lambda_S - lambda_D; 
-    double half_img_weight = 600; 
-    std::vector<cv::Rect> rectangles = {
-     cv::Rect(10, 10, 100, 50),
-     cv::Rect(50, 30, 80, 70),
-     cv::Rect(20, 40, 60, 30)
-    };
-    double I_lb = calculate_I_lb(Ri,Rj, half_img_weight, lambda_S, lambda_D, lambda_U, tao_S, rectangles);  // может сгенерировать исключение
-} 
+    vector<int> vector_max = { 0, 0 };
 
+    int total_sum = 0;
 
+    for (int r = 1; r < stats.rows; ++r) {
+        total_sum += stats.row(r).at<int>(4); // Площадь прямоугольника из структуры stats
+    }
 
+    bool right_and_left = false;
+
+    for (int i = 0; i < stats.rows; ++i) {
+        for (int j = i + 1; j < stats.rows; ++j) {
+            if (i != j) {
+                if (abs(centroids.row(i).at<double>(1) - centroids.row(j).at<double>(1)) < tao_v) {
+                    //  c0out << "I'm here"; 
+                    double I_lb = calculate_I_lb(stats.row(i), centroids.row(i), stats.row(j), centroids.row(j), right_and_left, half_img_weight, lambda_S, lambda_D, lambda_U, tao_S, total_sum); // может сгенерировать исключение
+                    if (I_lb > max_ij || max_ij == -1) {
+                        max_ij = I_lb;
+                        vector_max[0] = i;
+                        vector_max[1] = j;
+                    }
+                }
+            }
+        }
+    }
+
+    Mat lateral_stats;
+
+    if (max_ij != -1) {
+        lateral_stats.push_back(stats.row(vector_max[0]));
+        lateral_stats.push_back(stats.row(vector_max[1]));
+    }
+    return lateral_stats;
+
+}
--- detection_and_classification_brake_lights/brake_lights_status_classification.cpp
+++ detection_and_classification_brake_lights/brake_lights_status_classification.cpp
@@ -34,33 +34,54 @@ void calculateMean(const cv::Mat& imagePart, double& L_means, double& a_means) {
     a_means = Arr_means[1];
 } 
 
-int main(int argc, char** argv)
-{
+void drawBoundingRectangles(cv::Mat& image, cv::Mat stats) {
+    for (int i = 0; i < stats.rows; ++i) {
+        cv::rectangle(image, cv::Point(stats.at<int>(i, cv::CC_STAT_LEFT), stats.at<int>(i, cv::CC_STAT_TOP)),
+            cv::Point(stats.at<int>(i, cv::CC_STAT_LEFT) + stats.at<int>(i, cv::CC_STAT_WIDTH),
+                stats.at<int>(i, cv::CC_STAT_TOP) + stats.at<int>(i, cv::CC_STAT_HEIGHT)),
+            cv::Scalar(0, 255, 0), 2);
+    }
+}
 
- //   Mat image = imread("C:\\IMG_1675.PNG"); 
-    Mat image = imread("C:\\testdrive1583149193.4818494.png"); 
+void filterStatsAndCentroids(const cv::Mat& stats, const cv::Mat& centroids, cv::Mat& filteredStats, cv::Mat& filteredCentroids, const cv::Mat& resizedImage) {
+    for (int r = 0; r < stats.rows; ++r) {
+        int width = stats.at<int>(r, cv::CC_STAT_WIDTH);
+        int height = stats.at<int>(r, cv::CC_STAT_HEIGHT);
+
+        if (!(width == resizedImage.cols && height == resizedImage.rows) && !(width == 1 && height == 1)) {
+            filteredStats.push_back(stats.row(r));
+            filteredCentroids.push_back(centroids.row(r));
+        }
+    }
+}
+
+int main(int argc, char** argv)
+{ 
+
+    //   Mat image = imread("C:\\IMG_1675.PNG"); 
+    Mat image = imread("C:\\testdrive1583149193.4818494.png");
 
     if (image.empty())
     {
         cout << "Image Not Found!!!" << endl;
         cin.get(); //wait for any key press 
         return -1;
-    } 
+    }
 
-    int new_width = 416;
+    int new_weidth = 416;
     int new_height = 416;
     Mat resized_img;
     //resize down
-    resize(image, resized_img, Size(new_width, new_height), INTER_LINEAR);
+    resize(image, resized_img, Size(new_weidth, new_height), INTER_LINEAR);
     // let's upscale the image using new  width and height
     imshow("Resized Down by defining height and width", resized_img);
 
     Mat Lab_image = convertToLab(resized_img);
 
-   // namedWindow("RGB2Lab image", cv::WINDOW_NORMAL); //WINDOW_AUTOSIZE
- //   imshow("RGB2Lab image", Lab_image); 
-    
-    detector(Lab_image); 
+    // namedWindow("RGB2Lab image", cv::WINDOW_NORMAL); //WINDOW_AUTOSIZE
+  //   imshow("RGB2Lab image", Lab_image); 
+
+   // detector(Lab_image);
 
     // Выделение каналов 
     vector<Mat> lab_channels(3);
@@ -70,38 +91,46 @@ int main(int argc, char** argv)
 
     const int L = 0;
     const int a = 1;
-    const int B = 2; 
+    const int B = 2;
 
- //   imshow("a channel", lab_channels[a]); 
+    //   imshow("a channel", lab_channels[a]); 
+
+       /*
+    Применение порогового значения Оцу
+       */
 
     Mat otsu_img;
-    double thresh = 0;
-    double maxValue = 255;
-
+    int thresh = 0;
+    int maxValue = 255;
     long double thres = cv::threshold(lab_channels[a], otsu_img, thresh, maxValue, THRESH_OTSU);
 
-    cout << "Otsu Threshold: " << thres << endl; 
+    cout << "Otsu Threshold: " << thresh << endl;
+    imshow("Image after Otsu Threshold", otsu_img);
 
-    imshow("Image after Otsu Threshold", otsu_img); 
-
-    // Применение анализа связанных компонентов
-    vector<double> labels, stats, centroids;
+    cv::Mat labels, stats, centroids;
     int numLabels = cv::connectedComponentsWithStats(otsu_img, labels, stats, centroids);
 
- /*   for (int i = 1; i < numLabels; ++i) {
-        cv::rectangle(resized_img, cv::Point(stats.at<int>(i, cv::CC_STAT_LEFT), stats.at<int>(i, cv::CC_STAT_TOP)),
-            cv::Point(stats.at<int>(i, cv::CC_STAT_LEFT) + stats.at<int>(i, cv::CC_STAT_WIDTH),
-                stats.at<int>(i, cv::CC_STAT_TOP) + stats.at<int>(i, cv::CC_STAT_HEIGHT)),
-            cv::Scalar(0, 255, 0), 2);
-    }*/
+   // drawBoundingRectangles(resized_img, stats); 
 
-    // Отображение исходного изображения с метками
+  //  cv::imshow("Исходное изображение с метками", resized_img);
+    //  int k[] = { stats.row(0).col(3) };
+    cout << "stats 3: " << stats.row(0).col(0) << " stats: " << stats << "centroids: " << centroids;
+    // cout << "stats.row(0).col(3) " << k[0] << "resized_img.cols " << resized_img.cols;
+
+    Mat filteredStats, filteredCentroids;
+    filterStatsAndCentroids(stats, centroids, filteredStats, filteredCentroids, resized_img);
+    cout << filteredStats; 
+
+    cv::Mat mm = stats.row(0); 
+
+    cout << "stats 0: " << mm.col(0) << " stats: " << stats.row(1).at<int>(0);
+
+    Mat lateral_stats = detector_new(filteredStats, filteredCentroids, new_weidth / 2); 
+
+    drawBoundingRectangles(resized_img, lateral_stats); 
     cv::imshow("Исходное изображение с метками", resized_img); 
 
-    cout << "labels: "  << " stats: " << stats << "centroids: " << centroids;
-
-    // Отображение исходного и выделенного изображения
-   // cv::imshow("Изображение с метками", outputImage); 
+  // cout << "lateral" << lateral_stats;
 
     // Features 
 
@@ -109,12 +138,12 @@ int main(int argc, char** argv)
     cv::Mat croppedL = lab_channels[L](rect).clone();
     cv::Mat croppedA = lab_channels[a](rect).clone();
 
-    double maxValueL, minValueL, meanValueL, pixelPercentageL, tao_L = 128, meanValueTotalL; 
+    double maxValueL, minValueL, meanValueL, pixelPercentageL, tao_L = 128, meanValueTotalL;
     double maxValuea, minValuea, meanValuea, pixelPercentagea, tao_A = 128, meanValueTotala;
 
     calculateFeaturesWithoutMean(croppedL, tao_L, maxValueL, minValueL, pixelPercentageL);
-    calculateFeaturesWithoutMean(croppedA, tao_A, maxValuea, minValuea, pixelPercentagea); 
-    calculateMean(Lab_image(rect), meanValueL, meanValuea); 
+    calculateFeaturesWithoutMean(croppedA, tao_A, maxValuea, minValuea, pixelPercentagea);
+    calculateMean(Lab_image(rect), meanValueL, meanValuea);
     calculateMean(Lab_image, meanValueTotalL, meanValueTotala);
 
     cout << "Max from function " << maxValueL << " Min from function " << minValueL << " PercentageL from function " << pixelPercentageL << '\n';
@@ -122,5 +151,5 @@ int main(int argc, char** argv)
 
     // Wait for any keystroke in the window  
     waitKey(0);
-    return 0; 
+    return 0;
 }
