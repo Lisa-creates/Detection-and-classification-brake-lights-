5c00e13839ca886c8516042c7d2132c2f10c524a
Lisa-creates <lizalive56@gmail.com>
2024-04-14 23:14:55 +0300

    Add a possibility reading dataset with training data, and also code refactoring

--- detection_and_classification_brake_lights/brake_lights_status_classification.cpp
+++ detection_and_classification_brake_lights/brake_lights_status_classification.cpp
@@ -5,18 +5,55 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>  
 
+#include <vector>
+#include <opencv2/opencv.hpp>
+
+
 #include <stdio.h>
 
-#include "brake_lights_detection.cpp"
+#include "brake_lights_detection.cpp" 
+// #include "brake_lights_classifcation.cpp" 
+#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING 1;
+#include <filesystem> 
+//namespace fs = std::filesystem;
+#include <experimental/filesystem> 
+
+namespace fs = std::experimental::filesystem;
+
+//std::experimental::filesystemstd::filesystem 
+
+
 
 using namespace cv;
 using namespace std; 
 
 Mat convertToLab(const Mat& image) {
     Mat Lab_image;
-    cvtColor(image, Lab_image, 45);
+    cvtColor(image, Lab_image, 45); 
     return Lab_image;
 } 
+
+Mat convertToHSV(const Mat& image) {
+    Mat Lab_image;
+    cvtColor(image, Lab_image, 41); 
+    return Lab_image;
+} 
+
+void img_preprocessing(Mat& image, Mat& channel, const int num_channel, const int weight, const int height) {
+
+    resize(image, image, Size(weight, height), INTER_LINEAR);
+
+    Mat Lab_image = convertToLab(image); 
+
+    // Выделение каналов 
+    vector<Mat> lab_channels(3);
+
+    split(Lab_image, lab_channels); 
+
+    channel = lab_channels[num_channel];
+}
+
+
 
 void calculateFeaturesWithoutMean(const cv::Mat& imagePart, double threshold,
     double& maxValue, double& minValue, double& pixelPercentage) {
@@ -43,24 +80,54 @@ void drawBoundingRectangles(cv::Mat& image, cv::Mat stats) {
     }
 }
 
-void filterStatsAndCentroids(const cv::Mat& stats, const cv::Mat& centroids, cv::Mat& filteredStats, cv::Mat& filteredCentroids, const cv::Mat& resizedImage) {
+void filterStatsAndCentroids(const cv::Mat& stats, const cv::Mat& centroids, cv::Mat& filteredStats, cv::Mat& filteredCentroids, cv::Mat& resizedImage) {
     for (int r = 0; r < stats.rows; ++r) {
-        int width = stats.at<int>(r, cv::CC_STAT_WIDTH);
+        int wight = stats.at<int>(r, cv::CC_STAT_WIDTH);
         int height = stats.at<int>(r, cv::CC_STAT_HEIGHT);
 
-        if (!(width == resizedImage.cols && height == resizedImage.rows) && !(width == 1 && height == 1) && stats.at<int>(r, cv::CC_STAT_AREA) >= 0) { // 
+        int wight_img = resizedImage.cols; 
+        int height_img = resizedImage.rows; 
+
+        cout << wight_img << " " << height_img << endl; 
+
+        if (/*!(wight == wight_img && height == wight_img) &&*/ !(wight == 1 && height == 1) && stats.at<int>(r, cv::CC_STAT_AREA) >= 80 && stats.at<int>(r, cv::CC_STAT_AREA) <= wight_img * height_img / 3) { // 
             filteredStats.push_back(stats.row(r));
             filteredCentroids.push_back(centroids.row(r));
         }
     }
+} 
+
+void get_rectangle_for_detector(const Mat channel,cv::Mat& filteredStats,cv::Mat& filteredCentroids, cv::Mat& resized_img) {
+    Mat otsu_img;
+    int thresh = 0;
+    int maxValue = 255;
+    long double thres = cv::threshold(channel, otsu_img, thresh, maxValue, THRESH_OTSU);
+
+  // cout << "Otsu Threshold: " << thresh << endl;
+    imshow("Image after Otsu Threshold", otsu_img);
+
+    Mat labels, stats, centroids; 
+    int numLabels = cv::connectedComponentsWithStats(otsu_img, labels, stats, centroids);
+
+    // drawBoundingRectangles(resized_img, stats); 
+    //  cv::imshow("Исходное изображение с метками", resized_img); 
+    cout << "Befor filtr" << stats << endl;
+
+    filterStatsAndCentroids(stats, centroids, filteredStats, filteredCentroids, resized_img);
+    cout << "Befor filtr" << filteredStats << endl; 
+
+    drawBoundingRectangles(resized_img, filteredStats);
 }
 
 int main(int argc, char** argv)
 { 
 
    // Mat image = imread("C:\\testdrive1583149193.4818494.png"); 
-   // Mat image = imread("C:\\testdrive1583149150.6489925.png"); // красная 
-    Mat image = imread("C:\\testdrive1583149177.9194167.png");
+  //  Mat image = imread("C:\\testdrive1583149150.6489925.png"); // красная 
+  // Mat image = imread("C:\\testdrive1583149169.625285.png"); // красная с включёнными 
+  //  Mat image = imread("C:\\testdrive1583149177.9194167.png"); // белая 
+   
+    Mat image = imread("C:\\red_on.png");
 
     if (image.empty())
     {
@@ -69,93 +136,92 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    int new_weidth = 416;
-    int new_height = 416;
-    Mat resized_img;
-    //resize down
-    resize(image, resized_img, Size(new_weidth, new_height), INTER_LINEAR);
-    // let's upscale the image using new  width and height
-    imshow("Resized Down by defining height and width", resized_img);
-
-    Mat Lab_image = convertToLab(resized_img);
-
-    // namedWindow("RGB2Lab image", cv::WINDOW_NORMAL); //WINDOW_AUTOSIZE
-  //   imshow("RGB2Lab image", Lab_image); 
-
-   // detector(Lab_image);
-
-    // Выделение каналов 
-    vector<Mat> lab_channels(3);
-
-    split(Lab_image, lab_channels);
-
-
+    int new_weight = 416; 
+    int new_height = 416; 
     const int L = 0;
     const int a = 1;
     const int B = 2;
 
-    //   imshow("a channel", lab_channels[a]); 
+  /*  vector<string> input_folders_test = {"TRAIN/brake_TRAIN_OFF", "TRAIN/brake_ON_TRAIN"};
+    vector<vector<int>> features_test;
+    vector<int> labels_test; 
 
-       /*
+
+
+    for (const string& folder : input_folders_test) {
+        for (const auto& entry : fs::directory_iterator{ folder }) {
+            string image_path = entry.path().string(); 
+            Mat img = imread(image_path);
+            
+            Mat channel; 
+
+            img_preprocessing(img, channel, a, new_weight, new_height); 
+
+            Mat filteredStats, filteredCentroids; 
+
+            get_rectangle_for_detector(channel, filteredStats, filteredCentroids, image);
+
+         //   imshow("Resized Down by defining height and width", img);
+
+           
+            if (folder.substr(folder.size() - 3) == "OFF") {
+                labels_test.push_back(0);
+            }
+            else {
+                labels_test.push_back(1);
+            }
+        }
+    }*/ 
+
+    Mat resized_img = image.clone();
+    Mat channel;
+
+    img_preprocessing(image, channel, a, new_weight, new_height);
+
+    imshow("a", image);
+    imshow("a channel", channel); 
+
+       /* 
     Применение порогового значения Оцу
        */
 
-    Mat otsu_img;
-    int thresh = 0;
-    int maxValue = 255;
-    long double thres = cv::threshold(lab_channels[a], otsu_img, thresh, maxValue, THRESH_OTSU);
 
-    cout << "Otsu Threshold: " << thresh << endl;
-    imshow("Image after Otsu Threshold", otsu_img);
+    Mat filteredStats, filteredCentroids; 
+    get_rectangle_for_detector(channel, filteredStats, filteredCentroids, image);
 
-    cv::Mat labels, stats, centroids;
-    int numLabels = cv::connectedComponentsWithStats(otsu_img, labels, stats, centroids);
 
-   // drawBoundingRectangles(resized_img, stats); 
+    Mat lateral_stats = detector_new(filteredStats, filteredCentroids, new_weight / 2); 
 
-  //  cv::imshow("Исходное изображение с метками", resized_img);
-    //  int k[] = { stats.row(0).col(3) };
-    cout << "stats 3: " << stats.row(0).col(0) << " stats: " << stats << "centroids: " << centroids;
-    // cout << "stats.row(0).col(3) " << k[0] << "resized_img.cols " << resized_img.cols;
+    drawBoundingRectangles(image, lateral_stats); 
+    cv::imshow("Исходное изображение с метками", image);
 
-    Mat filteredStats, filteredCentroids;
-    filterStatsAndCentroids(stats, centroids, filteredStats, filteredCentroids, resized_img);
-    cout << filteredStats; 
+    cout << "lateral " << lateral_stats;
 
- //   drawBoundingRectangles(resized_img, filteredStats);
 
-    cv::Mat mm = stats.row(0); 
-
-    cout << "stats 0: " << mm.col(0) << " stats: " << centroids << endl; 
-
- //   Point offset = centroids.row(0) - centroids.row(1); 
-
-  //  cout << offset
-
-    Mat lateral_stats = detector_new(filteredStats, filteredCentroids, new_weidth / 2); 
-
-    drawBoundingRectangles(resized_img, lateral_stats); 
-    cv::imshow("Исходное изображение с метками", resized_img); 
-
-    cout << "lateral" << lateral_stats;
 
     // Features 
 
-    cv::Rect rect(0, 0, Lab_image.cols / 2, Lab_image.rows / 2); // Область фар
-    cv::Mat croppedL = lab_channels[L](rect).clone();
-    cv::Mat croppedA = lab_channels[a](rect).clone();
+   /* for (int i = 0; i < lateral_stats.rows; ++i) { 
 
-    double maxValueL, minValueL, meanValueL, pixelPercentageL, tao_L = 128, meanValueTotalL;
-    double maxValuea, minValuea, meanValuea, pixelPercentagea, tao_A = 128, meanValueTotala;
+        Mat current_rectangle = lateral_stats.row(i);
+        
+        cv::Rect R_i(current_rectangle.at<int>(cv::CC_STAT_LEFT), current_rectangle.at<int>(cv::CC_STAT_TOP), current_rectangle.at<int>(cv::CC_STAT_WIDTH), current_rectangle.at<int>(cv::CC_STAT_HEIGHT));
+        // Область фар 
 
-    calculateFeaturesWithoutMean(croppedL, tao_L, maxValueL, minValueL, pixelPercentageL);
-    calculateFeaturesWithoutMean(croppedA, tao_A, maxValuea, minValuea, pixelPercentagea);
-    calculateMean(Lab_image(rect), meanValueL, meanValuea);
-    calculateMean(Lab_image, meanValueTotalL, meanValueTotala);
+        cv::Mat croppedL = lab_channels[L](R_i).clone();
+        cv::Mat croppedA = lab_channels[a](R_i).clone();
 
-    cout << "Max from function " << maxValueL << " Min from function " << minValueL << " PercentageL from function " << pixelPercentageL << '\n';
-    cout << "Mean_L " << meanValueL << "Mean_a " << meanValuea << '\n';
+        double maxValueL, minValueL, meanValueL, pixelPercentageL, tao_L = 128, meanValueTotalL;
+        double maxValuea, minValuea, meanValuea, pixelPercentagea, tao_A = 128, meanValueTotala;
 
+        calculateFeaturesWithoutMean(croppedL, tao_L, maxValueL, minValueL, pixelPercentageL);
+        calculateFeaturesWithoutMean(croppedA, tao_A, maxValuea, minValuea, pixelPercentagea);
+        calculateMean(Lab_image(R_i), meanValueL, meanValuea);
+        calculateMean(Lab_image, meanValueTotalL, meanValueTotala);
+
+        cout << "Max from function " << maxValueL << " Min from function " << minValueL << " PercentageL from function " << pixelPercentageL << '\n';
+        cout << "Mean_L " << meanValueL << "Mean_a " << meanValuea << '\n';
+    } */
     // Wait for any keystroke in the window  
     waitKey(0);
     return 0;
