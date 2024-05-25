27e8db75632cda4ab9823ffd97bb0b79b6a28a9f
Lisa-creates <lizalive56@gmail.com>
2024-05-26 01:13:16 +0300

    Code refactoring and add comments

--- detection_and_classification_brake_lights/brake_lights_classification.cpp
+++ detection_and_classification_brake_lights/brake_lights_classification.cpp
@@ -1,73 +1,107 @@
-#include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/ml.hpp>
+#include"Header_files/brake_lights_classification.h"
+
+/**
+ * \brief Рассчитывает фичи для прямоугольной области изображения 
+ *
+ * \param current_rectangle Прямоугольная область изображения
+ * \param lab_channels Вектор каналов LAB цветового пространства
+ * \param tao_L Порог для канала L
+ * \param tao_A Порог для канала a
+ * \param Lab_image Изображение в цветовом пространстве LaB 
+ *
+ * \return Матрица с рассчитанными характеристиками.
+ */
+Mat calculate_features(const Mat& current_rectangle, const vector<Mat>& lab_channels, const double tao_L, const double tao_A, const Mat& Lab_image);
+
+ /**
+  * \brief Заполняет входные матрицы data_l, data_r и data_third фичами для левой, правой и третьей фары соответственно,
+  * полученными фичами из calculate_features 
+  * 
+  * \param data_l Матрица данных для левого стоп-сигнала
+  * \param data_r Матрица данных для правого стоп-сигнала
+  * \param data_third Матрица данных для третьего стоп-сигнала(если есть, если нет, заполняется фичами левого стоп-сигнала) 
+  * \param stats Координаты прямоугольников левого, правого и третьего стоп-сигнала
+  * \param channels Вектор каналов изображения
+  * \param img Изображение 
+  */
+void classifier_get_features(Mat& data_l, Mat& data_r, Mat& data_third, const Mat& stats, const vector<Mat> channels, Mat& img); 
+
+ /**
+  * \brief Выводит метрики (accuracy, recall, precision F2-меру) на основе предсказанных и истинных меток классов.
+  *
+  * \param testLabels Метки классов для тестирования.
+  * \param predictedLabels Предсказанные метки классов.
+  * \param type_sample Тип выборки (например, "тестовая для третьего сьоп-сигнала" или "обучающая для боковых стоп-сигналов").
+  */
+void print_metrics(const Mat& testLabels, const Mat& predictedLabels, const char* type_sample); 
+
+/**
+ * \brief Обучает модель SVM на данных data_third для третьего стоп-сигнала 
+ *
+ * \param data_third Матрица данных (для третьего стоп-сигнала) для обучения модели SVM.
+ * \param data_third_test Матрица данных для тестирования.
+ * \param trainLabels Метки классов для обучения модели.
+ * \param testLabels Метки классов для тестирования модели.
+ */
+void SVM_classifier_third_light(const Mat& data_third, const Mat& trainLabels, const Mat& data_third_test, const Mat& testLabels, Mat& predictedLabels, Mat& predictedLabels_train); 
 
 
-using namespace cv; 
-using namespace cv::ml;
-using namespace std; 
+/**
+ * \brief Основная функция классификации, объединяющая результаты двух предсказаний и вычисляющая окончательное предсказание.
+ *
+ * \param predict_LR Результаты предсказания для боковых фар
+ * \param predict_third Результаты предсказания третьей фары
+ * \param labels Метки классов для сравнения предсказаний 
+ */
+void main_classifier(const Mat& predict_LR, const Mat& predict_third, const Mat& labels);
 
 
-
-struct Features {
-    double maxValueL;
-    double minValueL;
-    double pixelPercentageL;
-    double meanValueL;
-    double maxValuea;
-    double minValuea;
-    double pixelPercentagea;
-    double meanValuea;
-};
-
-void calculateFeaturesWithoutMean(const cv::Mat& imagePart, double threshold,
+void calculate_features_without_mean(const Mat& imagePart, double threshold,
     float& max_, float& min_, float& pixelPercentage) {
 
     double maxValue, minValue; 
 
-    cv::minMaxLoc(imagePart, &minValue, &maxValue);
+    minMaxLoc(imagePart, &minValue, &maxValue);
 
     max_ = static_cast<float>(maxValue); 
     min_ = static_cast<float>(minValue); 
 
-    cv::Mat mask = imagePart > threshold;
-    pixelPercentage = 100.0 * cv::countNonZero(mask) / (imagePart.rows * imagePart.cols);
+    Mat mask = imagePart > threshold;
+    pixelPercentage = 100.0 * countNonZero(mask) / (imagePart.rows * imagePart.cols);
 }
 
-void calculateMean(const cv::Mat& imagePart, float& L_means, float& a_means) {
-    cv::Scalar Arr_means, Arr_stddev;
+void calculate_mean(const Mat& imagePart, float& L_means, float& a_means) {
+    Scalar Arr_means, Arr_stddev;
     meanStdDev(imagePart, Arr_means, Arr_stddev);
     L_means = Arr_means[0];
     a_means = Arr_means[1];
 } 
 
-Mat features(const Mat current_rectangle, const vector<Mat> lab_channels, const double tao_L, const double tao_A, const Mat Lab_image) {
+Mat calculate_features(const Mat& current_rectangle, const vector<Mat>& lab_channels, const double tao_L, const double tao_A, const Mat& Lab_image) {
     // Mat current_rectangle = lateral_stats.row(i);
     const int L = 0;
     const int a = 1;
     const int B = 2;
 
-    cv::Rect R_i(current_rectangle.at<int>(cv::CC_STAT_LEFT), current_rectangle.at<int>(cv::CC_STAT_TOP), current_rectangle.at<int>(cv::CC_STAT_WIDTH), current_rectangle.at<int>(cv::CC_STAT_HEIGHT));
+    Rect R_i(current_rectangle.at<int>(CC_STAT_LEFT), current_rectangle.at<int>(CC_STAT_TOP), current_rectangle.at<int>(CC_STAT_WIDTH), current_rectangle.at<int>(CC_STAT_HEIGHT));
     // Область фар 
 
-    cv::Mat croppedL = lab_channels[L](R_i).clone();
-    cv::Mat croppedA = lab_channels[a](R_i).clone();
+    Mat croppedL = lab_channels[L](R_i).clone();
+    Mat croppedA = lab_channels[a](R_i).clone();
 
     float maxValueL, minValueL, meanValueL, pixelPercentageL, meanValueTotalL;
     float maxValuea, minValuea, meanValuea, pixelPercentagea, meanValueTotala;
 
-    calculateFeaturesWithoutMean(croppedL, tao_L, maxValueL, minValueL, pixelPercentageL);
-    calculateFeaturesWithoutMean(croppedA, tao_A, maxValuea, minValuea, pixelPercentagea);
-    calculateMean(Lab_image(R_i), meanValueL, meanValuea);
-    calculateMean(Lab_image, meanValueTotalL, meanValueTotala);
+    calculate_features_without_mean(croppedL, tao_L, maxValueL, minValueL, pixelPercentageL);
+    calculate_features_without_mean(croppedA, tao_A, maxValuea, minValuea, pixelPercentagea);
+    calculate_mean(Lab_image(R_i), meanValueL, meanValuea);
+    calculate_mean(Lab_image, meanValueTotalL, meanValueTotala);
 
-    vector<float> feat = { maxValueL, minValueL,  pixelPercentageL, maxValuea, minValuea, pixelPercentagea,  meanValueL, meanValuea, meanValueTotalL, meanValueTotala };
+   // vector<float> feat = { maxValueL, minValueL,  pixelPercentageL, maxValuea, minValuea, pixelPercentagea,  meanValueL, meanValuea, meanValueTotalL, meanValueTotala };
 
-    cv::Mat new_row = (cv::Mat_<float>(1, 10) << maxValueL, minValueL, pixelPercentageL, maxValuea, minValuea, pixelPercentagea, meanValueL, meanValuea, meanValueTotalL, meanValueTotala);
+    Mat new_row = (Mat_<float>(1, 10) << maxValueL, minValueL, pixelPercentageL, maxValuea, minValuea, pixelPercentagea, meanValueL, meanValuea, meanValueTotalL, meanValueTotala);
 
-    Mat feat_mat = Mat(feat).reshape(10, 1);
+   // Mat feat_mat = Mat(feat).reshape(10, 1);
 
     // cout << "Max from function " << maxValueL << " Min from function " << minValueL << " PercentageL from function " << pixelPercentageL << '\n';
    //  cout << "Mean_L " << meanValueL << "Mean_a " << meanValuea << '\n'; 
@@ -75,28 +109,28 @@ Mat features(const Mat current_rectangle, const vector<Mat> lab_channels, const 
     return new_row; 
 } 
 
-void classifier_get_features(Mat& data_l, Mat& data_r, Mat& data_third, Mat& stats, const vector<Mat> channels, Mat& img) {
+void classifier_get_features(Mat& data_l, Mat& data_r, Mat& data_third, const Mat& stats, const vector<Mat> channels, Mat& img) {
 
     const int l = 0, r = 1, third = 2;
     const double tao_L = 150, tao_A = 155; 
 
     if (stats.rows >= 2) {
-        data_l.push_back(features(stats.row(l), channels, tao_L, tao_A, img));
-        data_r.push_back(features(stats.row(r), channels, tao_L, tao_A, img)); 
+        data_l.push_back(calculate_features(stats.row(l), channels, tao_L, tao_A, img));
+        data_r.push_back(calculate_features(stats.row(r), channels, tao_L, tao_A, img)); 
 
         
         if (stats.rows == 3) {
-            data_third.push_back({ features(stats.row(third), channels, tao_L, tao_A, img) });
+            data_third.push_back({ calculate_features(stats.row(third), channels, tao_L, tao_A, img) });
         } 
         else {
-            data_third.push_back({ features(stats.row(l), channels, tao_L, tao_A, img) }); 
+            data_third.push_back({ calculate_features(stats.row(l), channels, tao_L, tao_A, img) }); 
         }
     }
 } 
 
 
 
-void print_metrics(Mat testLabels, Mat predictedLabels, const char* type_sample) {
+void print_metrics(const Mat& testLabels, const Mat& predictedLabels, const char* type_sample) {
     Mat confusionMatrix = Mat::zeros(2, 2, CV_32S); 
     for (int i = 0; i < testLabels.rows; ++i) {
         int trueLabel = testLabels.at<int>(i, 0);
@@ -119,30 +153,15 @@ void print_metrics(Mat testLabels, Mat predictedLabels, const char* type_sample)
         }
         double precision = truePositives / static_cast<double>(truePositives + falsePositives);
         double recall = truePositives / static_cast<double>(truePositives + falseNegatives);
-        double F1 = 2 * ((precision * recall) / (precision + recall)); 
+        double F2 = 5 * ((precision * recall) / (4 * precision + recall));
         double accuracy = (truePositives + trueNegatives) / static_cast<double>(truePositives + falseNegatives + trueNegatives + falsePositives); 
-        cout << "Class " << i << " - Accuracy: " << accuracy << ", Precision: " << precision << ", Recall: " << recall << ", F1: " << F1 << endl;
+        cout << "Class " << i << " - Accuracy: " << accuracy << ", Precision: " << precision << ", Recall: " << recall << ", F2: " << F2 << endl;
     } 
-
-    int truePositives = confusionMatrix.at<int>(1, 1);
-    int falsePositives = confusionMatrix.at<int>(0, 1);
-    int falseNegatives = confusionMatrix.at<int>(1, 0); 
-    int trueNegatives = confusionMatrix.at<int>(0, 0); 
-
-    double precision = truePositives / static_cast<double>(truePositives + falsePositives);
-    double recall = truePositives / static_cast<double>(truePositives + falseNegatives);
-    cout << " - Precision: " << precision << ", Recall: " << recall << endl;
 }
 
 
 
-void SVM_classifier_LR_light(Mat data_l, Mat data_r, Mat trainLabels, Mat data_l_test, Mat data_r_test, Mat testLabels, Mat& predictedLabels, Mat& predictedLabels_train) {
-
-    // cout << data_third;
-
-
-
-    // cout << "Here" << trainLabelsMat;
+void SVM_classifier_LR_light(const Mat& data_l, const Mat& data_r, const Mat& trainLabels, const Mat& data_l_test, const Mat& data_r_test, const Mat& testLabels, Mat& predictedLabels, Mat& predictedLabels_train) {
 
  /*   Mat dataMat(data_l.rows + data_r.rows, data_l.cols, CV_32F);
     for (int i = 0; i < dataMat.rows; ++i) {
@@ -160,7 +179,7 @@ void SVM_classifier_LR_light(Mat data_l, Mat data_r, Mat trainLabels, Mat data_l
             trainLabelsMat.at<int>(i, 0) = trainLabels.at<int>(i - trainLabels.rows, 0);
     }
 */
-    Mat dataMat; 
+    Mat dataMat;
     cv::vconcat(data_l, data_r, dataMat); // первый массив записывается после 2 
 
     cout << "data_l.rows " << data_l.rows << " dataMat.rows " << dataMat.rows << endl;
@@ -168,106 +187,42 @@ void SVM_classifier_LR_light(Mat data_l, Mat data_r, Mat trainLabels, Mat data_l
 
 
     Mat trainLabelsMat(trainLabels.rows * 2, trainLabels.cols, CV_32S);
- /*   for (int i = 0; i < trainLabelsMat.rows; ++i) {
-        if (i < trainLabels.rows)
-            trainLabelsMat.at<int>(i, 0) = trainLabels.at<int>(i, 0);
-        else
-            trainLabelsMat.at<int>(i, 0) = trainLabels.at<int>(i - trainLabels.rows, 0);
-    }*/ 
+    /*   for (int i = 0; i < trainLabelsMat.rows; ++i) {
+           if (i < trainLabels.rows)
+               trainLabelsMat.at<int>(i, 0) = trainLabels.at<int>(i, 0);
+           else
+               trainLabelsMat.at<int>(i, 0) = trainLabels.at<int>(i - trainLabels.rows, 0);
+       }*/
 
-    cv::vconcat(trainLabels, trainLabels, trainLabelsMat); 
-    
+    cv::vconcat(trainLabels, trainLabels, trainLabelsMat);
+
 
     Mat dataMat_test(data_l_test.rows + data_r_test.rows, data_l_test.cols, CV_32F);
-    
+
     cv::vconcat(data_l_test, data_r_test, dataMat_test);
 
-    // cout << dataMat_test << endl;
 
-
-     // Set up SVM for OpenCV 3
     Ptr<SVM> svm = SVM::create();
-    // Set SVM type
     svm->setType(SVM::C_SVC);
-    // Set SVM Kernel to Radial Basis Function (RBF)
     svm->setKernel(SVM::RBF);
-    // Set parameter C 
 
-
-    // Train SVM on training data
     Ptr<TrainData> td = TrainData::create(dataMat, ROW_SAMPLE, trainLabelsMat);
 
-    int kFold = 5;
-
-    svm->trainAuto(td, kFold);
-
+    int kFold = 5; 
+    svm->trainAuto(td, kFold); 
     svm->predict(dataMat, predictedLabels_train);
 
- 
-    print_metrics(trainLabelsMat, predictedLabels_train, "train");
+    print_metrics(trainLabelsMat, predictedLabels_train, "train lateral light");
 
-
-    // Save trained model
     svm->save("svm_model_lateral.yml");
 
-  //  Mat predictedLabels;
     svm->predict(dataMat_test, predictedLabels);
 
-    print_metrics(testLabels, predictedLabels, "test"); 
-
-} 
-
-
-void SVM_classifier_LR_light2(Mat data_l, Mat data_r, Mat trainLabels, Mat data_l_test, Mat data_r_test, Mat testLabels, Mat& predictedLabels, Mat& predictedLabels_train) {
-
-    Mat dataMat;
-    cv::hconcat(data_l, data_r, dataMat); // первый массив записывается после 2 
-
-    cout << "data_l.rows " << data_l.rows << " dataMat.rows " << dataMat.rows << endl;
-
-    Mat trainLabelsMat(trainLabels.rows, trainLabels.cols, CV_32S);
-
-    trainLabelsMat = trainLabels; 
-
-    Mat dataMat_test(data_l_test.rows, data_l_test.cols * 2, CV_32F);
-
-    cv::hconcat(data_l_test, data_r_test, dataMat_test);
-
-    cout << "dataMat_test" << endl;
-
-
-     // Set up SVM for OpenCV 3
-    Ptr<SVM> svm = SVM::create();
-    // Set SVM type
-    svm->setType(SVM::C_SVC);
-    // Set SVM Kernel to Radial Basis Function (RBF)
-    svm->setKernel(SVM::RBF);
-    // Set parameter C 
-
-
-    // Train SVM on training data
-    Ptr<TrainData> td = TrainData::create(dataMat, ROW_SAMPLE, trainLabelsMat);
-
-    svm->trainAuto(td);
-
-    svm->predict(dataMat, predictedLabels_train);
-
-
-
-    print_metrics(trainLabelsMat, predictedLabels_train, "train");
-
-
-    // Save trained model
-    svm->save("svm_model_lateral.yml");
-
-    //  Mat predictedLabels;
-    svm->predict(dataMat_test, predictedLabels);
-
-    print_metrics(testLabels, predictedLabels, "test");
+    print_metrics(testLabels, predictedLabels, "test lateral light"); 
 }
 
 
-void SVM_classifier_third_light(Mat data_third, Mat trainLabels, Mat data_third_test, Mat testLabels, Mat& predictedLabels, Mat& predictedLabels_train) {
+void SVM_classifier_third_light(const Mat& data_third, const Mat& trainLabels, const Mat& data_third_test, const Mat& testLabels, Mat& predictedLabels, Mat& predictedLabels_train) {
 
     // cout << data_third;
 
@@ -304,11 +259,6 @@ void SVM_classifier_third_light(Mat data_third, Mat trainLabels, Mat data_third_
 
     svm->setKernel(SVM::RBF);
 
-    svm->setC(12.5);
-
-    svm->setGamma(0.50625);
-
-    // Train SVM on training data
     Ptr<TrainData> td = TrainData::create(data_third, ROW_SAMPLE, trainLabels);
 
     svm->trainAuto(td); 
@@ -316,27 +266,23 @@ void SVM_classifier_third_light(Mat data_third, Mat trainLabels, Mat data_third_
     svm->predict(data_third, predictedLabels_train); 
 
     print_metrics(trainLabels, predictedLabels_train, "train th");
-
-    // Save trained model
     svm->save("svm_model_third.yml");
 
 
-    // Predict on training data
-  //  Mat predictedLabels;
     svm->predict(data_third_test, predictedLabels);
 
     print_metrics(testLabels, predictedLabels, "test th");
 } 
 
 
-void main_classifier(Mat predict_LR, Mat predict_third, Mat labels) {
+void main_classifier(const Mat& predict_LR, const Mat& predict_third, const Mat& labels) {
 
     Mat predict(labels.rows, labels.cols, CV_32S);
 
     for (int i = 0; i < labels.rows; ++i) { // + predict_LR.at<int>(i + labels.rows, 0) 
         if (predict_LR.rows > labels.rows) {
          //   cout << i << " " << predict_LR.at<float>(i) + predict_third.at<float>(i) + predict_LR.at<float>(i + labels.rows) << endl;
-            if (predict_LR.at<float>(i) + predict_third.at<float>(i) + predict_LR.at<float>(i + labels.rows) >= 3)
+            if (predict_LR.at<float>(i) + predict_third.at<float>(i) + predict_LR.at<float>(i + labels.rows) >= 2)
                 predict.at<float>(i) = 1;
             else
                 predict.at<float>(i) = 0;
@@ -349,8 +295,6 @@ void main_classifier(Mat predict_LR, Mat predict_third, Mat labels) {
                 predict.at<float>(i) = 0;
         }
     }
-
-  // cout << "res " << predict;
 
    print_metrics(labels, predict, "test all");
 } 
